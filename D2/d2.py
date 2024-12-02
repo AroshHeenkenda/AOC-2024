25cd02f7d85727609812fbaf24d8a75788abfe65
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-02 21:14:56 +1100

    Completed day 2 part 2, also made alterations to part 1.

--- D2/d2.py
+++ D2/d2.py
@@ -1,26 +1,60 @@
 
+def check(lst):
+    l = [(lst[i] - lst[i-1]) for i in range(1, len(lst))]
+    return ((set(l) <= {1, 2, 3}) or (set(l) <= {-1, -2, -3}))
+
 def part1(fname):
     
     f = open(fname, "r")
-    reports = []
-
-    for line in f.readlines():
-        l = [int(num) for num in line.strip().split()]
-        dif = [(l[i] - l[i-1]) for i in range(1, len(l))]
-        reports.append(dif)
-
-    f.close() 
     safe = 0
 
-    for r in reports:
-        if (set(r) <= {1, 2, 3}) or (set(r) <= {-1, -2, -3}):
+    for line in f.readlines():
+        levels = [int(num) for num in line.strip().split()]
+        if check(levels):
             safe += 1
 
+    f.close() 
     return safe
 
 
 def part2(fname):
-    return None
+
+    f = open(fname, "r")
+    safe = 0
+
+    for line in f.readlines():
+
+        levels = [int(num) for num in line.strip().split()]
+        # Safe
+        if check(levels):
+            safe += 1
+        # Do another check
+        else:
+            for i in range(len(levels)):
+                f_levels = [lvl for l, lvl in enumerate(levels) if l != i]
+                if check(f_levels):
+                    safe += 1
+                    break
+
+    f.close() 
+    return safe
+
+
+    # for report in reports:
+
+    #     r = dif(report)
+    #     # It is safe
+    #     if check(r):
+    #         safe += 1
+    #     else:
+    #         for i in range(len(r)):
+    #             new_report = [el for e, el in enumerate(report) if e != i]
+    #             new_r = dif(new_report)
+    #             if check(new_r):
+    #                 safe += 1
+    #                 break
+
+
 
 if __name__ == "__main__":
     
