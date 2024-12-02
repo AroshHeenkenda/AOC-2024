8bd1282b86357570dd3a3ecc53370d118e646a76
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-02 20:48:08 +1100

    Completed day 2 part 1.

--- D2/d2.py
+++ D2/d2.py
@@ -5,15 +5,16 @@ def part1(fname):
     reports = []
 
     for line in f.readlines():
-
         l = [int(num) for num in line.strip().split()]
+        dif = [(l[i] - l[i-1]) for i in range(1, len(l))]
+        reports.append(dif)
 
-        for i in range(1, len(l)):
-            pass
-
-    print(reports)    
-
+    f.close() 
     safe = 0
+
+    for r in reports:
+        if (set(r) <= {1, 2, 3}) or (set(r) <= {-1, -2, -3}):
+            safe += 1
 
     return safe
 
