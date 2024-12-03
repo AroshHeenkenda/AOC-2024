e539595014c5d8a10e2089799087e49170dfadcb
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-03 21:45:56 +1100

    Completed day 3 part 1.

--- D3/d3.py
+++ D3/d3.py
@@ -1,13 +1,35 @@
+import re
+
+def calc_mul(str):
+
+    nums = (str[4:])[:-1]
+    n1, n2 = nums.split(",")
+    return (int(n1) * int(n2))
+
 
 def part1(fname):
-    pass
+    
+    # Regex pattern for mul(x{xx},x{xx})
+    pat = re.compile('mul\(\d{1,3},\d{1,3}\)')
+
+    f = open(fname, "r")
+    code = ''.join(f.readlines())
+    f.close()
+    
+    matches = pat.findall(code)
+    sum = 0
+
+    for m in matches:
+        sum += calc_mul(m)
+
+    return sum
 
 def part2(fname):
     pass
 
 if __name__ == "__main__":
     
-    print("AOC 2024 DAY XX")
+    print("AOC 2024 DAY 3")
     out1 = part1("input.txt")
     print(f"Part 1: {out1}")
 
