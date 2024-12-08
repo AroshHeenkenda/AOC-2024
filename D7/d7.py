3cd3b6f8a31433b787eabe885dcec6db6ea53abb
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-09 10:14:03 +1100

    Complete day 7 part 1, also included sample input for testing.

--- D7/d7.py
+++ D7/d7.py
@@ -1,13 +1,58 @@
+from collections import defaultdict
+
+def parseinput(fname):
+
+    f = open(fname, "r")
+    lines = f.readlines()
+    f.close()
+
+    out = []
+
+    for line in lines:
+        target, nums = line.strip().split(": ")
+        out.append(tuple([int(target), list(map(int, nums.split()))]))
+    
+    return out
+
+def calibrate(nums):
+    n = len(nums)
+
+    dp = []
+    dp.append([nums[0]])
+
+    for i in range(1, n):
+        cur = nums[i]
+        next_dp = []
+
+        for d in dp[i-1]:
+            next_dp.append(d * cur)
+            next_dp.append(d + cur)
+        
+        dp.append(set(next_dp))
+    
+    return dp
+
+
 
 def part1(fname):
-    pass
+
+    equations = parseinput(fname)
+    sum_eq = 0
+
+    for target, nums in equations:
+        c = calibrate(nums)
+        if target in c[-1]:
+            sum_eq += target
+
+    return sum_eq
+
 
 def part2(fname):
-    pass
+    pass    
 
 if __name__ == "__main__":
     
-    print("AOC 2024 DAY XX")
+    print("AOC 2024 DAY 7")
     out1 = part1("input.txt")
     print(f"Part 1: {out1}")
 
