9f00fc4c9261a9149ac429a97b6254c94b02bca4
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-09 14:09:00 +1100

    Add templates for day 8 part 1, includes puzzle input and description.

---
+++ D8/d8.py
@@ -0,0 +1,15 @@
+
+def part1(fname):
+    pass
+
+def part2(fname):
+    pass
+
+if __name__ == "__main__":
+    
+    print("AOC 2024 DAY XX")
+    out1 = part1("input.txt")
+    print(f"Part 1: {out1}")
+
+    out2 = part2("input.txt")
+    print(f"Part 2: {out2}")
