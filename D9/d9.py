a34f3ab1bb2e8bd611a279c5d370b3d7da2a5623
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-09 21:31:30 +1100

    Complete day 9 part 1, also included description of part 2.

--- D9/d9.py
+++ D9/d9.py
@@ -1,6 +1,58 @@
 
+def parseinput(fname):
+
+    f = open(fname, "r")
+    line = f.readline().strip()
+    f.close()
+
+    id = 0
+    ind = 0
+    out = []
+
+    while ind != len(line) - 1:
+
+        file = [id] * int(line[ind])
+        free = ["."] * int(line[ind + 1])
+        out += file + free
+
+        ind += 2
+        id += 1
+    
+    out += [id] * int(line[len(line)-1])
+    return out
+
+
 def part1(fname):
-    pass
+    
+    space = parseinput(fname)
+
+    # 2 pointer approach
+    l, r = 0, len(space) - 1
+    while l < r:
+
+        # Move left to find space
+        if space[l] != ".":
+            l += 1
+        
+        # Move right to find file
+        if space[r] == ".":
+            r -= 1
+        
+        # Swap if both in right pos
+        if (space[l] == ".") and (space[r] != "."):
+            space[l], space[r] = space[r], space[l]
+            l += 1
+            r -= 1
+
+    checksum = 0
+    for i in range(len(space)):
+        val = space[i]
+        if val == ".":
+            break
+        checksum += i * int(val)
+
+    return checksum
+
 
 def part2(fname):
     pass
