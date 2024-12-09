d4eb5be40b1694eb5325ed6baf84ddd72b0c4460
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2024-12-09 16:58:46 +1100

    Add template files for day 9 part 1.

--- D8/d8.py
+++ D8/d8.py
@@ -39,20 +39,51 @@ def calc_anti(ant1, ant2):
     anti2 = ((2*ant2[0] - ant1[0], 2*ant2[1] - ant1[1]))
     return anti1, anti2
 
+def calc_anti_2(ant1, ant2, m):
+
+    antis = []
+
+    a1 = ant1
+    a2 = ant2
+    # backwards
+    while True:
+        x = 2*a1[0] - a2[0] 
+        y = 2*a1[1] - a2[1]
+
+        if valid_coord((x, y), m):
+            antis.append((x, y))
+            a2 = a1
+            a1 = (x, y)
+        else:
+            break
+
+    a1 = ant1
+    a2 = ant2
+    # backwards
+    while True:
+        x = 2*a2[0] - a1[0] 
+        y = 2*a2[1] - a1[1]
+
+        if valid_coord((x, y), m):
+            antis.append((x, y))
+            a1 = a2
+            a2 = (x, y)
+        else:
+            break
+
+    return antis
+
 
 def part1(fname):
-    
     m, antenna = parseinput(fname)
     anti_nodes = []
 
     for ant in antenna.keys():
-
         a = antenna[ant]
 
         # Get combinations to get anti nodes
         for i in range(len(a) - 1):
             for j in range(i+1, len(a)):
-
                 # Get anti nodes
                 anti1, anti2 = calc_anti(a[i], a[j])
                 
@@ -67,9 +98,23 @@ def part1(fname):
     return len(set(anti_nodes))
 
 
-
 def part2(fname):
-    pass
+    m, antenna = parseinput(fname)
+    anti_nodes = []
+
+    for ant in antenna.keys():
+        a = antenna[ant]
+
+        # Get combinations to get anti nodes
+        for i in range(len(a) - 1):
+            for j in range(i+1, len(a)):
+                new_antis = calc_anti_2(a[i], a[j], m)
+                anti_nodes += new_antis
+
+    print(anti_nodes)
+
+    # Want unique so return set of anti nodes
+    return len(set(anti_nodes))
 
 if __name__ == "__main__":
     
@@ -77,5 +122,5 @@ if __name__ == "__main__":
     out1 = part1("input.txt")
     print(f"Part 1: {out1}")
 
-    out2 = part2("input.txt")
+    out2 = part2("test.txt")
     print(f"Part 2: {out2}")
